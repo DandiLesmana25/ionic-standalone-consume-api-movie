cf40906cae1f1ce67c5c69992e71608fb2d6bfbe
DandiLesmana25 <dandilesmana.if21@gmail.com>
2026-03-10 22:57:23 +0700

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ion-standalone-consume-api-movie',
+  webDir: 'www'
+};
+
+export default config;
