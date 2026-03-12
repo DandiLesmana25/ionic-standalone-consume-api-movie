9fa909010c23ae2bbc937bb1fdee7c76d73589de
DandiLesmana25 <dandilesmana.if21@gmail.com>
2026-03-12 09:10:36 +0700

    write consume api top rated movie

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, inject, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, InfiniteScrollCustomEvent, IonList, IonItem, IonAvatar, IonSkeletonText, IonAlert, IonLabel, IonButtons, IonButton, IonIcon, IonBadge,  IonMenuButton,
+ } from '@ionic/angular/standalone';
+import { Movie } from '../services/movie';
+import { catchError, finalize } from 'rxjs';
+import { DatePipe } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MovieResult } from '../services/interfaces';
+
+import { addIcons } from 'ionicons';
+import { create, ellipsisHorizontal, ellipsisVertical, helpCircle, personCircle, search, star } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  // standalone: true,
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonAvatar, IonSkeletonText, IonAlert, RouterModule, DatePipe, IonLabel, IonButtons, IonButton, IonIcon, IonBadge,  IonMenuButton,],
 })
-export class HomePage {
-  constructor() {}
+
+
+export class HomePage implements OnInit {
+  private movieService = inject(Movie);
+  private currentPage = 1;
+  public error = null;
+  public isLoading = false;
+  public movies: MovieResult[] = [];
+  public imageBaseUrl = 'https://image.tmdb.org/t/p';
+   public dummyArray = new Array(5);
+
+     ngOnInit() {
+    this.loadMovies();
+  }
+
+
+
+
+  constructor() {
+    this.loadMovies();
+     addIcons({ create, ellipsisHorizontal, ellipsisVertical, helpCircle, personCircle, search, star });
+  }
+
+  async loadMovies(event?: InfiniteScrollCustomEvent) {
+   this.error = null;
+
+   if (!event) {
+     this.isLoading  = true;
+   }
+
+   this.movieService.getTopRatedMovie(this.currentPage).pipe(
+    finalize(() => {
+      this.isLoading = false;
+   
+    }),
+    catchError((err : any) => {
+      console.log(err);
+      
+      this.error = err.error.status_message;
+      return [];
+    })
+   ).subscribe({
+    next: (res) => {
+      this.movies.push(...res.results);
+       if (event) {
+      event.target.disabled = res.total_pages === this.currentPage;
+        }
+    }
+
+   
+   })
+  }
+
+
+  loadMore(event: InfiniteScrollCustomEvent) {
+this.currentPage++;
+    this.loadMovies(event);
+  }
 }
--- src/app/services/movie.ts
+++ src/app/services/movie.ts
@@ -1,9 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
+import { delay, Observable } from 'rxjs';
+import { MovieResult, ApiResult } from './interfaces';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = environment.apiKey;
+
 
 @Injectable({
   providedIn: 'root',
@@ -13,11 +16,14 @@ export class Movie {
 private http = inject(HttpClient);
   constructor() {}
 
-  getTopRatedMusic(page = 1) {
-    return this.http.get(`${BASE_URL}/movie/pupular?page=${page}&api_key=${API_KEY}`)
+  getTopRatedMovie(page = 1): Observable<ApiResult>  {
+    // return this.http.get(`${BASE_URL}/movie/pupular?page=${page}&api_key=${API_KEY}`)
+    return this.http.get<ApiResult>(`${BASE_URL}/movie/popular?page=${page}&api_key=${API_KEY}`).pipe(
+      delay(1000) )
+    
   }
 
-  getMovieDetails(id: string) {
-    return this.http.get(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`)
+  getMovieDetails(id: string):  Observable<MovieResult>  {
+    return this.http.get<MovieResult>(`${BASE_URL}/movie/${id}?api_key=${API_KEY}`)
   }
 }
